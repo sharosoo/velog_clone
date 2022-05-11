d40230f68c6014dfefa0389b0f437e9a8dfb27ce
sharosoo <42034323+sharosoo@users.noreply.github.com>
2022-05-11 12:55:29 +0900

    add description on comment model

--- velog/comments/models.py
+++ velog/comments/models.py
@@ -13,13 +13,20 @@ class Comment(MP_Node, TimeStampedModel):
     사이트에서 허용 가능한 총 (root) 댓글 수 (게시물 수와 동일) : 10 ** 12
     게시물당 허용 가능한 댓글 수 : 10 ** 12
     """
-    # mp_node 적용 이유 ForeignKey -> 'self'로 하는 경우 대댓글 구조를 다시
-    node_order_by = ['created']
-    steplen = 8
     # article마다 root 댓글을 하나 생성후 article에서 root 댓글을 foreign key로 가진다.
-    # comment는 그 자체를 쿼리하는 경우는 없고 article에 종속적인 개념이다.
     # 그 아래에 모든 댓글을 트리 형태로 관리한다.
+    # mp_node 적용 이유 ForeignKey -> 'self'로 대댓글 구조를 구현하는 것은 어렵다고 생각했다.
+    # 댓글은 article detail view에서 필요한데 comment에서 root node를 제외하고 path를 기준으로 정렬하면 모든 댓글이 순서에 따라 표현된다.
+    # 1
+    # 1/1, 1/2
+    # 2
+    # 2/1, 2/2
+    # ...
+    node_order_by = ['created']
+    # root 댓글의 수 (= 게시물의 수) = (허용 가능한 char) ** (steplen)이다.
+    steplen = 8
 
+    # profile이 삭제되면 그 profile이 작성한 게시물도 없어져야 한다.
     profile = models.ForeignKey(
         UserProfile,
         on_delete=models.CASCADE

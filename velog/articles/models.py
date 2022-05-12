c22257410e2f1e93068042bd0a8a92f2990eeeee
sharosoo <42034323+sharosoo@users.noreply.github.com>
2022-05-12 15:03:19 +0900

    add verbose_name on article model field

--- velog/articles/models.py
+++ velog/articles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models import Max
+from django.utils.functional import cached_property
 
 from django_extensions.db.models import TimeStampedModel
 
@@ -17,11 +18,13 @@ class Article(TimeStampedModel):
     profile = models.ForeignKey(
         UserProfile,
         on_delete=models.CASCADE,
-        related_name='article'
+        related_name='article',
+        verbose_name='프로필'
     )
 
     title = models.CharField(
         max_length=60,
+        verbose_name='제목'
     )
 
     # URL source에서 article의 주소를 사람이 읽을 수 있는 형태로 고치기 위해 slugify를 이용한다.
@@ -32,24 +35,29 @@ class Article(TimeStampedModel):
         max_length=60,
         # blank=True,
         # default = slugify function()
+        verbose_name='슬러그'
     )
 
     # Todo: editor 구현을 위해 markdownx 필드로 변경 필요
     # 구현을 쉽게 하기 위해 우선은 TextField로 두자. markdownx필드는 TextField기반이므로 변경이 쉬울 것으로 예상된다.
-    content = models.TextField()
+    content = models.TextField(
+        verbose_name='내용'
+    )
 
     # view_cnt는 항상 0이상
     # 사용자가 입력하면 안됨 readonly
     view_cnt = models.PositiveIntegerField(
         blank=True,
-        default=0
+        default=0,
+        verbose_name='조회수'
     )
 
     # series_order를 통해 series내의 게시물 순서를 결정한다. (0: series 아님, 1~ : 각자의 series에서 series order)
     # Todo: 노출순서 설정 더 고민해보기, Manager로 잘 감싸자
     series_order = models.PositiveIntegerField(
         blank=True,
-        default=0
+        default=0,
+        verbose_name='시리즈 순서'
     )
 
     # series가 삭제된다 하더라도 그 하위에 있던 게시물은 삭제가 되면 안된다.
@@ -58,7 +66,8 @@ class Article(TimeStampedModel):
         on_delete=models.SET_NULL,
         related_name='article',
         blank=True,
-        null=True
+        null=True,
+        verbose_name='시리즈'
     )
 
     # article에 달린 comment(트리구조)의 root를 가리킨다.
@@ -70,9 +79,9 @@ class Article(TimeStampedModel):
         related_name='article',
         editable=False,
         blank=True,
-        null=True
+        null=True,
+        verbose_name='댓글'
     )
-    # TODO: unique constraints - (profile, title)
 
     # article 모델에서 comment를 관리해야하므로 그다지 좋지 않은 구조인 것 같다.
     def save(self, **kwargs):

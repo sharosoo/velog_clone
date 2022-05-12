1d43b6ba52f0bda5cbffcc2fbf091e01427f5f8c
sharosoo <42034323+sharosoo@users.noreply.github.com>
2022-05-12 18:25:44 +0900

    Update bulk_create_accounts.py
    add sha256 on user password

--- velog/accounts/bulk_create_accounts.py
+++ velog/accounts/bulk_create_accounts.py
@@ -9,7 +9,7 @@ User = get_user_model()
 
 def bulk_create_user(number_of_user):
     for i in range(number_of_user):
-        user = User.objects.create(
+        user = User.objects.create_user(
             username=f'test_user_{str(uuid.uuid4())[:8]}',
             password=str(uuid.uuid4())
         )

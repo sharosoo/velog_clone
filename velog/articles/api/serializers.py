e3bcedb2297668de52be55331b2b79a398819cb3
sharosoo <42034323+sharosoo@users.noreply.github.com>
2022-05-13 13:43:47 +0900

    remove tags field in article model serializer
    tag가 manytomany field로 내부적으로 pk로 검색해서 받아오는 동작이 오류를 일으켜 잠시 삭제

--- velog/articles/api/serializers.py
+++ velog/articles/api/serializers.py
@@ -7,7 +7,10 @@ from ..models import Article
 
 class ArticleDetailSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField()
+    # queryset을 이용해 JSON안에 JSON 형태로 만들고 싶었으나 이렇게 설정하는 경우 출력이 dict 형태로 나옴.
     comments = serializers.JSONField(source='get_related_comments_dict', read_only=True)
+    # Todo: tags도 같이 보내기
+    #  many to many field를 직접 serialize하는 것은 pk관련 오류가 발생하여 이후 수정 필요
 
     class Meta:
         model = Article
@@ -25,5 +28,4 @@ class ArticleDetailSerializer(serializers.ModelSerializer):
             'series_order',
             'root_comment',
             'comments',
-            'tags'
         ]
